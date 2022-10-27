e8dbcb9a48f115a78c1bf254cdca899d35cd28d8
Sai Teja Madha <smadha1@asu.edu>
2022-10-27 11:36:08 -0700

    Create Urinals.java

---
+++ src/main/java/Urinals.java
@@ -0,0 +1,21 @@
+/**
+ * @author Sai Teja Madha
+ */
+public class Urinals {
+
+    void getString() {}
+
+    void openFile() {}
+
+    void countUrinals() {}
+
+    boolean goodString(String str) {
+        System.out.println("Not yet implemented");
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
