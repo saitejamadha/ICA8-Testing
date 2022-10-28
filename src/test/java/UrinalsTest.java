ccf01d51c5459649e3b4ba0d47bcb66a9a91f3f8
Sai Teja Madha <smadha1@asu.edu>
2022-10-27 20:16:58 -0700

    remaining tests for count urinals

--- src/test/java/UrinalsTest.java
+++ src/test/java/UrinalsTest.java
@@ -17,12 +17,38 @@ public class UrinalsTest {
     }
 
     @Test
-    void testCountUrinals() {
+    void testCountUrinals1() {
+        System.out.println(" ==== TEST 1 EXECUTED ==== ");
         Assertions.assertEquals(-1, urinals.countUrinals("10011"));
+    }
+
+    @Test
+    void testCountUrinals2() {
+        System.out.println(" ==== TEST 2 EXECUTED ==== ");
         Assertions.assertEquals(1, urinals.countUrinals("10001"));
+    }
+
+    @Test
+    void testCountUrinals3() {
+        System.out.println(" ==== TEST 3 EXECUTED ==== ");
         Assertions.assertEquals(0, urinals.countUrinals("1001"));
+    }
+
+    @Test
+    void testCountUrinals4() {
+        System.out.println(" ==== TEST 4 EXECUTED ==== ");
         Assertions.assertEquals(3, urinals.countUrinals("00000"));
+    }
+
+    @Test
+    void testCountUrinals5() {
+        System.out.println(" ==== TEST 5 EXECUTED ==== ");
         Assertions.assertEquals(2, urinals.countUrinals("0000"));
+    }
+
+    @Test
+    void testCountUrinals6() {
+        System.out.println(" ==== TEST 6 EXECUTED ==== ");
         Assertions.assertEquals(1, urinals.countUrinals("01000"));
     }
 
