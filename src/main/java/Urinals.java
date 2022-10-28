fca5ca124e33775cfc1a47e53ea9b0c61811ffdb
Sai Teja Madha <smadha1@asu.edu>
2022-10-27 17:44:41 -0700

    implemented countUrinals method

--- src/main/java/Urinals.java
+++ src/main/java/Urinals.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Sai Teja Madha
  */
@@ -7,7 +11,42 @@ public class Urinals {
 
     void openFile() {}
 
-    void countUrinals() {}
+    int countUrinals(String urinals) {
+        Pattern pattern = Pattern.compile("11");
+        Matcher matcher = pattern.matcher(urinals);
+        if (matcher.find()) {
+            return -1;
+        }
+
+        int previousCount = (int) urinals.chars().filter(ch -> ch == '1').count();
+
+        int[] urinalsArray = new int[urinals.length()];
+        for (int i = 0; i < urinals.length(); i++) {
+            urinalsArray[i] = Character.getNumericValue(urinals.charAt(i));
+        }
+
+        for (int i = 0; i < urinalsArray.length; i++) {
+            int urinal = urinalsArray[i];
+            if (i == 0) {
+                if (urinalsArray.length == 1) {
+                    return 1 - Integer.parseInt(String.valueOf(urinal));
+                } else if ((urinal == 0) && urinalsArray[i + 1] == 0) {
+                    urinalsArray[i] = 1;
+                }
+            } else if (i == urinalsArray.length - 1) {
+                if (urinal == 0 && urinalsArray[i - 1] == 0) {
+                    urinalsArray[i] = 1;
+                }
+            } else {
+                if (urinal == 0 && urinalsArray[i - 1] == 0 && urinalsArray[i + 1] == 0) {
+                    urinalsArray[i] = 1;
+                }
+            }
+        }
+
+        return (int) Arrays.stream(urinalsArray).filter(x -> x == 1).count() - previousCount;
+
+    }
 
     boolean goodString(String str) {
         System.out.println("Not yet implemented");
@@ -15,7 +54,6 @@ public class Urinals {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
 
 }
--- src/test/java/UrinalsTest.java
+++ src/test/java/UrinalsTest.java
@@ -1,12 +1,29 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UrinalsTest {
+
+    private Urinals urinals;
+
+    @BeforeEach
+    public void setUp() {
+        urinals = new Urinals();
+    }
 
     @Test
     void testJunit() {
         Assertions.assertEquals("Sai Teja Madha", "Sai Teja Madha");
     }
 
+    @Test
+    void testCountUrinals() {
+        Assertions.assertEquals(-1, urinals.countUrinals("10011"));
+        Assertions.assertEquals(1, urinals.countUrinals("10001"));
+        Assertions.assertEquals(0, urinals.countUrinals("1001"));
+        Assertions.assertEquals(3, urinals.countUrinals("00000"));
+        Assertions.assertEquals(2, urinals.countUrinals("0000"));
+        Assertions.assertEquals(1, urinals.countUrinals("01000"));
+    }
 
 }
