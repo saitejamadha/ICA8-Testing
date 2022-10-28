517b54bf960cd031c8ae4c4d5382a1f61b684105
Sai Teja Madha <smadha1@asu.edu>
2022-10-27 17:19:28 -0700

    added UrinalsTest.java with example test

---
+++ src/test/java/UrinalsTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UrinalsTest {
+
+    @Test
+    void testJunit() {
+        Assertions.assertEquals("", "Sai Teja Madha");
+    }
+
+
+}
